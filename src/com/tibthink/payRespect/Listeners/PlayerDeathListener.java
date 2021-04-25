4d7272b6992d39fc77707829f8c99cb05533efd4
Chipmunk <11890508+ChipmunkCraft@users.noreply.github.com>
2021-04-25 00:15:22 -0700

    Disabled Killer/Killed by Messages
    Disabled Killer/Killed by messages temporarily to avoid Null Pointer exception.  TODO: Check if player was killed by another player before trying to send messages.

--- src/com/tibthink/payRespect/Listeners/PlayerDeathListener.java
+++ src/com/tibthink/payRespect/Listeners/PlayerDeathListener.java
@@ -7,29 +7,26 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 import com.tibthink.payRespect.Main;
-import com.tibthink.payRespect.Utils.Utils;
+//import com.tibthink.payRespect.Utils.Utils;
 
 public class PlayerDeathListener implements Listener {
+    private static Main plugin;
 
-	private static Main plugin;
+    public PlayerDeathListener(Main plugin) {
+		      Bukkit.getPluginManager().registerEvents(this, plugin);
+    }
 
-	public PlayerDeathListener(Main plugin) {
-
-		Bukkit.getPluginManager().registerEvents(this, plugin);
-	}
-
-	@EventHandler
-	public void onPlayerDeath(PlayerDeathEvent e) {
-		if (e.getEntity().getPlayer() instanceof Player) {
-            Player killer = e.getEntity().getKiller();
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent e) {
+		      if (e.getEntity().getPlayer() instanceof Player) {
+            //Player killer = e.getEntity().getKiller();
             Player p = e.getEntity();
+			         Bukkit.broadcastMessage(plugin.getConfig().getString("deathMsg").replace("%player%", p.getDisplayName()));
 
-			Bukkit.broadcastMessage(plugin.getConfig().getString("deathMsg").replace("%player%", p.getDisplayName()));
-
+			         /*  This part is broken.  Produces Null pointer exception if player does not die by another player.  TODO: Test for killed by player or natural.
             killer.sendMessage(Utils.chat(plugin.getConfig().getString("killerMsg").replace("%player%", p.getDisplayName())));
-            p.sendMessage(Utils.chat(plugin.getConfig().getString("killedMsg").replace("%player%", killer.getDisplayName())));
-			return;
-		}
-	}
-
+            p.sendMessage(Utils.chat(plugin.getConfig().getString("killedMsg").replace("%player%", killer.getDisplayName())));*/
+            return;
+        }
+    }
 }
